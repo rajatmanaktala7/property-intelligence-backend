5104138f4a5aa4b00b623741e794a4c9f7fff58c
rajatmanaktala7 <rajatmanaktala7@gmail.com>
2026-09-21 01:52:08 +0530

    Fix matcher authority Query import and critical registration

--- alliance_master_requirement_authority_v1.py
+++ alliance_master_requirement_authority_v1.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 import html, json, re, time
 from urllib.parse import quote
-from fastapi import HTTPException, Request, Form
+from fastapi import HTTPException, Request, Form, Query
 from fastapi.responses import HTMLResponse, JSONResponse, RedirectResponse
 from sqlalchemy import inspect, text
 
